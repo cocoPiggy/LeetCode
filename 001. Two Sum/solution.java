b78b17734abce653fd637aa8d7d2a85415ab44c2
liqingqing5264 <liqingqing5264@gmail.com>
2018-01-08 01:35:27 +0800

    Q1

--- 001. Two Sum/solution.java
+++ 001. Two Sum/solution.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res=new int[2];
+        HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(m.containsKey(target-nums[i])){
+                res[1]=m.get(target-nums[i]);
+                res[0]=i;
+                break;
+            }
+            if(map.containsKey(numbers[i])) {
+                continue;
+            }
+            m.put(nums[i],i);
+        }
+        return res;
+    }
+}
