4833368d1b1f78bccaac1c5e299d4b95a73a9494
liqingqing5264 <liqingqing5264@gmail.com>
2018-01-08 01:05:20 +0800

    Q7

--- 007. Reverse Integer/solution.java
+++ 007. Reverse Integer/solution.java
@@ -1,1 +1,16 @@
-
+'''java
+class Solution {
+    public int reverse(int x) {
+        int res =0;
+        while(x!=0){
+            int temp=res*10+x%10;
+            x=x/10;
+            if(res!=temp/10){
+                return 0;
+            }
+            res=temp;
+        }
+        return res;
+    }
+}
+'''
